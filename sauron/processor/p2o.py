bd76db04b563d5b5660312eb4a179ed62b7c7c5f
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-07-20 11:18:32 +0530

    add sauron add db
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- sauron/processor/p2o.py
+++ sauron/processor/p2o.py
@@ -1,34 +1,16 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2015-2019 Bitergia
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Authors:
-#   Alvaro del Castillo San Felix <acs@bitergia.com>
-#
 
 import logging
 from datetime import datetime
-from os import sys
+import os
+import sys
 
 from grimoire_elk.elk import feed_backend, enrich_backend
 from grimoire_elk.elastic import ElasticSearch
 from grimoire_elk.elastic_items import ElasticItems
 from grimoire_elk.utils import get_params, config_logging
 
+P20_SCRIPT_PATH = str(os.path.realpath(__file__))
 
 if __name__ == "__main__":
     """Perceval2Ocean tool"""
