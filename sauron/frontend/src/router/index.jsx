6730ebd6a18a29ad50fe5930ee7a478c123676a5
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-07-21 22:07:03 +0530

    add frontend
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- sauron/frontend/src/router/index.jsx
+++ sauron/frontend/src/router/index.jsx
@@ -3,12 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import Error from "../pages/Error";
 import Home from "../pages/Home";
+import Header from "../components/Header";
+import Dashboard from "../pages/Dashboard";
 
 const Router = () => {
   return (
     <Suspense fallback={null}>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Suspense>
