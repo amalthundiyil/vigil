9bedee78ab86ebd182a42d511ac22f71cd3931e2
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-07-23 13:14:33 +0530

    fix frontend
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- sauron/frontend/src/App.js
+++ sauron/frontend/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Topbar from './Components/Topbar/Topbar';
-import Packageinfo from './Components/Info/PackageInfo';
-import PackageStats from './Components/Stats/PackageStats';
-import Community from './Components/Community/Community';
-import Home from './Components/Home/Home'
+import logo from "./logo.svg";
+import Topbar from "./Components/Topbar/Topbar";
+import Packageinfo from "./Components/Info/PackageInfo";
+import PackageStats from "./Components/Stats/PackageStats";
+import Community from "./Components/Community/Community";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
--- sauron/frontend/src/Components/Stats/PackageStats.jsx
+++ sauron/frontend/src/Components/Stats/PackageStats.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import {userData} from '../../dummydata'
-import Barchart from '../Charts/Barchart'
-import Chart from '../Charts/Chart'
-import './packagestats.css'
+import React from "react";
+import { userData } from "../../dummydata";
+import Barchart from "../Charts/Barchart";
+import Chart from "../Charts/Chart";
+import "./packagestats.css";
 
 function PackageStats() {
   return (
     <div>
-        <div className='chart-container'>
-            <Chart data={userData} title='Popularity' grid dataKey='Downloads'/>
-            <Barchart></Barchart>
-        </div>
+      <div className="chart-container">
+        <Chart data={userData} title="Popularity" grid dataKey="Downloads" />
+        <Barchart></Barchart>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PackageStats
+export default PackageStats;
