770748c39961b3477c3ccc80c50cb006bbf05c36
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-07-29 13:36:51 +0530

    frontend: fix axios post
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- sauron/frontend/src/components/SearchBar/index.jsx
+++ sauron/frontend/src/components/SearchBar/index.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import Input from "@mui/material/Input";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import IconButton from "@mui/material/IconButton";
 
-const SearchBar = ({ placeholder, onChange, searchBarWidth }) => {
+const SearchBar = ({ placeholder, onChange, onSubmit, searchBarWidth }) => {
   return (
     <Box sx={{ display: "flex", alignItems: "center" }}>
-      <SearchIcon sx={{ marginRight: "10px" }} />
-      <Input
-        placeholder={placeholder}
-        onChange={onChange}
-        sx={{
-          width: searchBarWidth,
-          color: "rgba(0, 0, 0, 0.6)",
-          fontSize: "1.1rem",
-        }}
-        disableUnderline
-      />
+      <form onSubmit={onSubmit}>
+        <IconButton aria-label="delete" type="submit">
+          <SearchIcon />
+        </IconButton>
+        <Input
+          id="search"
+          placeholder={placeholder}
+          onChange={onChange}
+          sx={{
+            width: searchBarWidth,
+            color: "rgba(0, 0, 0, 0.6)",
+            fontSize: "1.1rem",
+          }}
+          disableUnderline
+        />
+      </form>
     </Box>
   );
 };
