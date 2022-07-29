f7b0d0852043720dbb603b5f7f37e30b92ecb5d0
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-07-29 16:56:52 +0530

    navigate to dashboard
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- sauron/frontend/src/pages/Dashboard/index.jsx
+++ sauron/frontend/src/pages/Dashboard/index.jsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Spinner from "../../components/Spinner";
+import Box from "@mui/material/Box";
+import { useGlobalContext } from "../../context";
+import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import axios from "../../utils/axios";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -12,7 +17,15 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-export default function index() {
+export default function Dashboard() {
+  const { loading, setLoading, metrics, setMetrics } = useGlobalContext();
+
+  console.log(metrics);
+
+  if (loading) {
+    return <Spinner open={true} />;
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
