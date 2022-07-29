39d1c4dc1821f6c530876a86cf19aa6496309e44
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-07-29 16:30:28 +0530

    add particles to frontend
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- sauron/frontend/src/router/index.jsx
+++ sauron/frontend/src/router/index.jsx
@@ -6,9 +6,21 @@ import Home from "../pages/Home";
 import Header from "../components/Header";
 import Dashboard from "../pages/Dashboard";
 import axios from "../utils/axios";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const Router = () => {
   const [allPackages, setAllPackages] = useState([]);
+
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,6 +32,83 @@ const Router = () => {
   return (
     <Suspense fallback={null}>
       <Header />
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#eeeee4",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#C0C0C0",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home data={allPackages} />} />
         <Route path="/home" element={<Home data={allPackages} />} />
