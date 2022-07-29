8b94a5a9a11fc3170ac7274cd4849297719cd5ca
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-07-29 19:53:34 +0530

    ench: fix readme
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- sauron/backend/server/api/dashboard.py
+++ sauron/backend/server/api/dashboard.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 
 from flask import Blueprint, jsonify, request, current_app, make_response
 
-from sauron.backend.server.commands.dashboard import get_validated_class, full_process, final_summary
+from sauron.backend.server.commands.dashboard import get_validated_class, full_process, summary
 from sauron.cli.checks import DOMAINS
 
 
@@ -22,8 +22,7 @@ def post():
         data.append({"domain": domain, "data": d})
         print({"domain": domain, "data": d})
         LOG.info({"domain": domain, "data": d}) 
-    import pdb; pdb.set_trace()
-    final_score, final_desc = final_summary(data)
+    final_score, final_desc = summary(data)
     data.append({"final_score": final_score, "final_desc": final_desc})
     return jsonify(
         status_code=HTTPStatus.CREATED,
