dcee887ebf5757390236b17b003f4e9e22ffe210
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-07-31 12:30:39 +0530

    ench: add get from es db
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- sauron/backend/server/commands/dashboard.py
+++ sauron/backend/server/commands/dashboard.py
@@ -1,4 +1,8 @@
-from sauron.processor.base_processor import BaseProcessor, ValidationError, final_summary
+from sauron.processor.base_processor import (
+    BaseProcessor,
+    ValidationError,
+    final_summary,
+)
 from sauron.cli.checks_util import get_from_config
 from sauron.cli.checks import DOMAINS
 from sauron.backend.server.api.errors import (
@@ -16,11 +20,13 @@ def get_validated_class(domain, url=None, name=None, type=None, token=None):
     except ValidationError as e:
         raise BadRequestError(e.message)
 
+
 def full_process(p):
     score_data = p.process()
     summary_data = p.summarize()
     ts_data = p.server_ts()
     return {"score_data": score_data, "ts_data": ts_data, "summary": summary_data}
+
 
 def summary(data):
     scores = []
@@ -28,5 +34,19 @@ def summary(data):
         scores.append(data[domain]["summary"]["score"])
     return final_summary(scores)
 
+
 def get_package_info(p):
     return {"desc": p.backend.description, "type": p.type, "name": p.name, "url": p.url}
+
+
+def get_es_data(url=None, name=None, type=None, token=None):
+    from sauron.cli.db_utils import connect_es, get_data
+
+    p = BaseProcessor.get_processor_class("community", url, name, type, token)
+    es = connect_es()
+    if not es:
+        return
+    es_data = get_data(p.url, p.name, p.type, es)
+    if not es_data:
+        return
+    return es_data
