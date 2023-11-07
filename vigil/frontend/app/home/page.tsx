b34d8d3440c721ca73b954258e0533cc31041d9b
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2023-11-07 21:41:33 +0530

    fix(frontend): small screen, big text
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- vigil/frontend/app/home/page.tsx
+++ vigil/frontend/app/home/page.tsx
@@ -16,22 +16,20 @@ export default function HomePage() {
 
   return (
     <main className="h-[65vh] w-full justify-center items-center flex px-4 flex-col ">
-      <div className="hidden flex-col md:flex md-10">
-        <div className="flex h-16 flex-end px-4 w-screen">
-          <MainNav className="mx-6" />
-          <div className="ml-auto flex items-center space-x-4">
-            <Button variant="outline" asChild>
-              <Link href="https://github.com/amal-thundiyil/vigil">
-                <Icons.gitHub className="h-5 w-5" />
-              </Link>
-            </Button>
-            <Button onClick={() => signIn()}>Sign In</Button>
-          </div>
+      <div className="flex h-16 flex-end px-4 w-screen m-3">
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <Button variant="outline" asChild>
+            <Link href="https://github.com/amal-thundiyil/vigil">
+              <Icons.gitHub className="h-5 w-5" />
+            </Link>
+          </Button>
+          <Button onClick={() => signIn()}>Sign In</Button>
         </div>
       </div>
       <div className="scroll-m-20 text-2xl sm:text-3xl md:text-4xl font-extrabold tracking-normal lg:text-5xl text-center space-y-3 justify-center mt-40">
-        Software Supply Chain Protection {<br />}
-        <span className="flex gap-2">
+        Software Supply Chain Protection {<br className="hidden md:block" />}
+        <span className="gap-2 hidden md:flex">
           for
           <TextAnimation
             className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
